2f72e46c943cd3547f957964e7b99e6d9cbcc865
john <niklowitzjohn11@gmail.com>
2026-02-10 10:59:17 +0100

    DeepL anbindung

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
-from fastapi import FastAPI
+import os
 import requests
+import deepl
+import urllib3
+from fastapi import FastAPI
+from dotenv import load_dotenv
 
+# Warnings ausschalten
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# API Key laden / DEEPL anmeldung
+load_dotenv()
+auth_key = os.getenv("DEEPL_API_KEY")
+
+if not auth_key:
+    raise ValueError("Kein DeepL API Key angegeben.")
+
+deepl_client = deepl.DeepLClient(auth_key)
+
+# API initialisieren
 app = FastAPI()
 
+# Funktion zum Übersetzen
+def translate(englischer_text):
+    try:
+        deutscher_text = deepl_client.translate_text(englischer_text, target_lang="DE")
+
+    except Exception:
+        deutscher_text = f"(Übersetzung fehlgeschlagen) {englischer_text}"
+
+    return deutscher_text.text
+
+# Willkommensnachricht
 @app.get("/")
 def read_root():
     return {"message": "Willkommen bei meiner REST API! Geh zu /quote für ein Zitat."}
 
+# Random Zitat abrufen
 @app.get("/quote")
 def get_random_quote():
     url = "https://api.quotable.io/random"
@@ -14,11 +43,10 @@ def get_random_quote():
     response = requests.get(url, verify=False)
     data = response.json()
 
+    zitat = translate(data["content"])
+    autor = translate(data["author"])
+
     return {
-        "autor": data["author"],
-        "zitat": data["content"]
+        "autor": autor,
+        "zitat": zitat
     }
-
-
-# https://github.com/lukePeavey/quotable Zitat API
-# https://libretranslate.com/ Übersetzer API
